ff1f4bde6a63fd8f68e8bf8b1076c924c8c2a37f
toavina <toavinahajaina@gmail.com>
2023-06-20 17:01:23 +0300

    ADX 1007 - first draft

--- ape/forms.py
+++ ape/forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired, Email
 
 
 class UserDataForm(FlaskForm):
-    name = StringField('Full name', validators=[DataRequired()])
-    email = EmailField('Email', validators=[Email()])
-    orgname = StringField('Organisation name', validators=[DataRequired()])
-    jobtitle = StringField('Job title', validators=[DataRequired()])
-    submit = SubmitField('Save')
+    name = StringField('Full name', validators=[DataRequired()], render_kw={"class_": "form-control"})
+    email = EmailField('Email', validators=[Email()], render_kw={"class_": "form-control"})
+    orgname = StringField('Organisation name', validators=[DataRequired()], render_kw={"class_": "form-control"})
+    jobtitle = StringField('Job title', validators=[DataRequired()], render_kw={"class_": "form-control"})
+    submit = SubmitField('Save',  render_kw={"class_": "btn"})
