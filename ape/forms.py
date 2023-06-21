32d77c891192b07a155ca0298257cc1ca55b3573
toavina <toavinahajaina@gmail.com>
2023-06-21 11:27:44 +0300

    ADX 1007 - Added placeholders

--- ape/forms.py
+++ ape/forms.py
@@ -4,8 +4,8 @@ from wtforms.validators import DataRequired, Email
 
 
 class UserDataForm(FlaskForm):
-    name = StringField('Full name', validators=[DataRequired()], render_kw={"class_": "form-control"})
-    email = EmailField('Email', validators=[Email()], render_kw={"class_": "form-control"})
-    orgname = StringField('Organisation name', validators=[DataRequired()], render_kw={"class_": "form-control"})
-    jobtitle = StringField('Job title', validators=[DataRequired()], render_kw={"class_": "form-control"})
+    name = StringField('Full name', validators=[DataRequired()], render_kw={"class_": "form-control", "placeholder": "Full name"})
+    email = EmailField('Email', validators=[Email()], render_kw={"class_": "form-control", "placeholder": "Email"})
+    orgname = StringField('Organisation name', validators=[DataRequired()], render_kw={"class_": "form-control", "placeholder": "Organisation name"})
+    jobtitle = StringField('Job title', validators=[DataRequired()], render_kw={"class_": "form-control", "placeholder": "Job title"})
     submit = SubmitField('Save',  render_kw={"class_": "btn"})
