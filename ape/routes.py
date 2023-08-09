b81be13ba4f01e6364deaade6beaef96c63db814
Kamil Forenc <kamil.forenc@gmail.com>
2023-08-09 18:02:04 +0200

    Use different URLs for 'back' and 'after successful save' cases. Remove these URLs from session if they weren't passed to home page via URL parameters.

--- ape/routes.py
+++ ape/routes.py
@@ -1,6 +1,6 @@
 import logging
 
-from urllib.parse import quote_plus, urlencode, urlparse
+from urllib.parse import quote_plus, urlencode
 
 from authlib.integrations.flask_client import OAuth
 from flask import redirect, render_template, session, url_for, \
@@ -17,20 +17,27 @@ app_blueprint = Blueprint('main', __name__)
 
 @app_blueprint.route("/")
 def home():
-    return_url = request.args.get("return_url", None)
+
     lang = request.args.get("lang", None)
-    if return_url:
-        parsed_url = urlparse(return_url)
-        session['return_url'] = parsed_url.scheme + \
-            "://" + parsed_url.netloc + "/ape_data_receiver"
     if lang and lang in current_app.config['LANGUAGES']:
         session["lang"] = lang
+    set_or_clear_session_variable("back_url")
+    set_or_clear_session_variable("after_save_url")
     if session.get("user_id", ""):
         return redirect("/profile")
     else:
         return oauth.auth0.authorize_redirect(
             redirect_uri=url_for("main.callback", _external=True)
         )
+
+
+def set_or_clear_session_variable(variable_name):
+    value = request.args.get(variable_name, None)
+
+    if value:
+        session[variable_name] = value
+    else:
+        session.pop(variable_name, default=None)
 
 
 @app_blueprint.route("/profile", methods=['GET', 'POST'])
@@ -48,12 +55,14 @@ def profile():
     elif not form.is_submitted():
         form = logic.load_data_from_server_to_form(form, user_id)
 
-    url = session.get("return_url") if session.get("return_url", "") else None
+    back_url = session.get("back_url") if session.get("back_url", "") else None
+    after_save_url = session.get("after_save_url") if session.get("after_save_url", "") else None
 
     return render_template(
         "profile.html",
         form=form,
-        return_url=url
+        back_url=back_url,
+        after_save_url=after_save_url
     )
 
 
